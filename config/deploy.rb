a44f90392bf6692d829ed98745984424c72434d1
borimao <maonibori@gmail.com>
2019-11-06 16:56:25 +0900

    つかれた

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,10 +30,10 @@ set :rbenv_ruby, '2.6.3'
 #出力するログのレベル。
 set :log_level, :debug
 
-#set :sidekiq_role, :web
-set :pty,  true
-set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
-set :sidekiq_monit_conf_dir, '/etc/monit.d'
+set :sidekiq_role, :web
+#set :pty,  true
+#set :sidekiq_config, -> { File.join(shared_path, 'config', 'sidekiq.yml') }
+#set :sidekiq_monit_conf_dir, '/etc/monit.d'
 
 
 namespace :deploy do
